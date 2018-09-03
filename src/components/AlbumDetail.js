cd76240af48d3b6ac486da06cf401ccb82f1e685
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-09-03 16:11:53 -0300

    Add styles for header and image elements

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,26 +4,38 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
-  const { artist, title, thumbnail_image } = props.album;
-  const { headerContentStyle, thumbnailStyle } = styles;
+  const { artist, title, thumbnail_image, image } = props.album;
+  const {
+     headerContentStyle,
+     headerTextStyle,
+     thumbnailStyle,
+     thumbnailContainerStyle,
+     imageStyle
+  } = styles;
 
   return (
     <Card>
-      <CardSection>
 
-        <View>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
-
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
-
       </CardSection>
+
+      <CardSection>
+          <Image
+            style={imageStyle}
+            source={{ uri: image }}
+          />
+      </CardSection>
+
     </Card>
   );
 };
@@ -33,9 +45,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
